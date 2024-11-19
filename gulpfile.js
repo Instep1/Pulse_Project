3b27ed614e65f46cdfab137ff50cb7aad72d3663
Maksim <lvutinwork@gmail.com>
2024-11-19 23:13:24 +0300

    Check#2 - adjusted gulp.js, compilation scss to css is correct now

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,9 @@
-const gulp        = require('gulp');
+const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require("gulp-rename");
-// import gulp from 'gulp';
-// import autoprefixer from 'gulp-autoprefixer';
 const cleanCSS = require('gulp-clean-css');
 
-// Static server
 gulp.task('live-server', function() {
     browserSync.init({
         server: {
@@ -16,22 +13,19 @@ gulp.task('live-server', function() {
 });
 
 gulp.task('styles-sass', function() {
-    return gulp.src("src/sass/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
             .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
             .pipe(rename({
                 prefix: "",
                 suffix: ".min"
               }))
-            // .pipe(autoprefixer({
-            //     cascade: false
-            // }))
             .pipe(cleanCSS({compatibility: 'ie8'}))
             .pipe(gulp.dest("src/css"))
             .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function(){
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel('styles-sass'))
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles-sass'))
     gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
